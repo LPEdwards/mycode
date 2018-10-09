6209623c465b6f329efbe7af57843600b1426930
LPEdwards <laura.p.edwards@verizonwireless.com>
2018-10-09 09:22:51 -0400

    updating copycat script

---
+++ lab_input/input_ip.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Written By: Laura
+# Collect IP address information from a user and display it back to them.
+
+## Collect input from the user
+user_input = input('Please enter an IPv4 IP address:')
+
+## Print out the input collected from the user
+print("You told me the IPv4 address is:" + user_input)
+
+## Collect vendor name from the user
+user_input_vendor = input('Please enter the Vendors name:')
+
+## Print out the vendor input collected from the user
+print("You told me the Vendors name is:" + user_input_vendor)
